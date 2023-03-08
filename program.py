a632339a2926b9711afa4c9a238f665e8fef11cb
danielkean5 <124378867+danielkean5@users.noreply.github.com>
2023-03-08 15:46:26 +0000

    first try

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 # My Python Program
-# Task:  use if statements to output the result of the game fizzbuzz.  
+# Task:  use if statements to output the result of the game fizzbuzz.
 # Start at 1
 # For multiples of 3, output   Fizz
 # For multiples of 5, output   Buzz
@@ -7,7 +7,14 @@
 # End at 32
 
 result = ""
-for myNumber in range(32):
-  result = str(result) + str(myNumber) + "\n"
-
+for myNumber in range(1,33):
+    if myNumber%3 == 0:
+        if myNumber%5 == 0:
+            result = str(result) + str("FizzBuzz") + "\n"
+        else:
+            result = str(result) + str("Fizz") + "\n"
+    elif myNumber%5 == 0:
+        result = str(result) + str("Buzz") + "\n"
+    else:
+        result = str(result) + str(myNumber) + "\n"
 print(result)
